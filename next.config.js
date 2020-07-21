ef4f773e3d2ce07b9702387bb77f06439676d771
Brennan Fife <brenfife@gmail.com>
2020-07-21 13:30:59 -0500

    fleek config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
-const withImages = require('next-images');
+// const withImages = require('next-images');
 
 module.exports = {
     reactStrictMode: true,
     assetPrefix: '.',
     RPC_URL_4: 'https://rinkeby.infura.io/v3/60ab76e16df54c808e50a79975b4779f',
+    exportTrailingSlash: true,
+    exportPathMap: function () {
+        return {
+            '/': { page: '/' },
+        };
+    },
 };
 
-module.exports = withImages();
+// module.exports = withImages();
