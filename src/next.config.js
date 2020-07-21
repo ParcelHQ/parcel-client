7b48ada974f7a06ac4a4b2c0e6b6a6c5505bf1ad
Brennan Fife <brenfife@gmail.com>
2020-07-21 14:11:35 -0500

    fleek

--- src/next.config.js
+++ src/next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     assetPrefix: '.',
     RPC_URL_4: 'https://rinkeby.infura.io/v3/60ab76e16df54c808e50a79975b4779f',
     exportTrailingSlash: true,
-    exportPathMap: function () {
+    exportPathMap: () => {
         return {
             '/': { page: '/' },
         };
